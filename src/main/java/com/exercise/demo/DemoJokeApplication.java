47a6cab22915a764e5f6b2c74184c6e178b6ebf3
gustavonvp <gustavonvp@gmail.com>
2021-07-18 14:41:58 -0300

    Creating Spring Boot Project

---
+++ src/main/java/com/exercise/demo/DemoJokeApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJokeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJokeApplication.class, args);
+    }
+
+}
